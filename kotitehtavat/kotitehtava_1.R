8dafc1ed0afb870446a4cc9e217d49f000b7c9a1
Markus Kainu <markuskainu@gmail.com>
2016-08-11 11:01:25 +0300

    Kotitehtäväpohjat

---
+++ kotitehtavat/kotitehtava_1.R
@@ -0,0 +1,20 @@
+#' ---
+#' title: Kotitehtävä 1
+#' author: Etunimi Sukunimi
+#' date: Palautuspäivämäärä tähän
+#' output: 
+#'  html_document: 
+#'      toc: true
+#'      toc_float: true
+#'      number_sections: yes
+#'      code_folding: show
+#'      theme: united
+#' ---
+#' 
+#' # Johdanto
+
+
+#' # Ohjelmistoympäristö
+
+
+#' # Versiohallinta
---
+++ kotitehtavat/kotitehtava_5.R
@@ -0,0 +1,12 @@
+#' ---
+#' title: Kotitehtävä 4
+#' author: Etunimi Sukunimi
+#' date: Palautuspäivämäärä tähän
+#' output: 
+#'  html_document: 
+#'      toc: true
+#'      toc_float: true
+#'      number_sections: yes
+#'      code_folding: show
+#'      theme: united
+#' ---
